401223972bee24c086550597f4a6e235d92716eb
Etourneau Gwenn <gwenn.etourneau@gmail.com>
2017-02-10 11:42:19 +0900

    Update build and push binary

--- lib/concourse-binary-builder.rb
+++ lib/concourse-binary-builder.rb
@@ -5,38 +5,34 @@ require_relative 'git-client'
 
 class ConcourseBinaryBuilder
 
-  attr_reader :binary_name, :git_ssh_key, :task_root_dir
+  attr_reader :dependency, :git_ssh_key, :task_root_dir
   attr_reader :binary_builder_dir, :built_dir, :builds_dir
-  attr_reader :builds_yaml_artifacts, :binary_artifacts_dir, :original_source_code_dir, :source_url
+  attr_reader :builds_yaml_artifacts, :binary_artifacts_dir, :source_url
   attr_reader :verification_type, :verification_value, :flags, :latest_build, :remaining_builds
   attr_reader :platform, :os_name
 
-  def initialize(binary_name, task_root_dir, git_ssh_key, platform, os_name)
+  def initialize(dependency, task_root_dir, git_ssh_key, platform, os_name)
     @platform = platform.to_sym
     @os_name = os_name.to_sym
     @git_ssh_key = git_ssh_key
-    @binary_name = binary_name
+    @dependency = dependency
     @task_root_dir = task_root_dir
     @binary_builder_dir = File.join(task_root_dir,'binary-builder')
     @built_dir = File.join(task_root_dir, 'built-yaml')
-    @builds_dir = File.join(task_root_dir ,'builds-yaml')
+    @builds_dir = File.join(task_root_dir,'builds-yaml')
     @builds_yaml_artifacts = File.join(task_root_dir, 'builds-yaml-artifacts')
     @binary_artifacts_dir = File.join(task_root_dir, 'binary-builder-artifacts')
-    @original_source_code_dir = File.join(binary_artifacts_dir, 'original-source-code')
-    FileUtils.mkdir_p(original_source_code_dir)
   end
 
   def run
     load_builds_yaml
 
     unless latest_build
-      puts "There are no new builds for #{binary_name} requested."
+      puts "There are no new builds for #{dependency} requested."
       exit
     end
 
     build_dependency
-
-    tar_dependency_source_code
 
     copy_binaries_to_output_directory
 
@@ -48,17 +44,17 @@ class ConcourseBinaryBuilder
   private
 
   def load_builds_yaml
-    builds_file = File.join(builds_dir, "#{binary_name}-builds.yml")
+    builds_file = File.join(builds_dir, 'binary-builds', "#{dependency}-builds.yml")
     builds = YAML.load_file(builds_file)
 
-    @latest_build = builds[binary_name].shift
+    @latest_build = builds[dependency].shift
     @remaining_builds = builds
 
     return if @latest_build.nil?
 
-    @flags = "--name=#{binary_name}"
+    @flags = "--name=#{dependency}"
     latest_build.each_pair do |key, value|
-      if key == 'md5' || key == 'sha256'
+      if key == 'md5' || key == 'sha256' || key == 'git-commit-sha'
         @verification_type = key
         @verification_value = value
       elsif key == 'gpg-signature'
@@ -70,9 +66,13 @@ class ConcourseBinaryBuilder
   end
 
   def build_dependency
-    if binary_name == "composer"
+    case dependency
+    when 'bower'
+      @source_url = "https://registry.npmjs.org/bower/-/bower-#{latest_build['version']}.tgz"
+      download_non_build_dependency(source_url, 'bower', 'tgz')
+    when 'composer'
       @source_url = "https://getcomposer.org/download/#{latest_build['version']}/composer.phar"
-      download_composer(source_url)
+      download_non_build_dependency(source_url, 'composer', 'phar')
     else
       binary_builder_output = run_binary_builder(flags)
       /- url:\s(.*)$/.match(binary_builder_output)
@@ -80,60 +80,47 @@ class ConcourseBinaryBuilder
     end
   end
 
-  def download_composer(url)
-      system("curl #{url} -o #{binary_builder_dir}/composer-#{latest_build['version']}.phar") or raise "Could not download composer.phar"
-  end
-
-  def tar_dependency_source_code
-    version_to_build = latest_build['version']
-
-    dependency_source = case binary_name
-                        when "composer" then "#{binary_builder_dir}/composer-#{version_to_build}.phar"
-                        when "glide" then "src/"
-                        when "godep" then "src/"
-                        else source_directory
-                        end
-
-    if binary_name == "composer"
-      system("tar -zcf #{binary_builder_dir}/build.tgz #{dependency_source}") or raise "Could not tar composer-#{version_to_build}.phar"
-    else
-      if Dir.exist?(File.join("/tmp",dependency_source))
-        system("tar -zcf #{binary_builder_dir}/build.tgz -C /tmp ./#{dependency_source}") or raise "Could not create tar"
-      else
-        raise "Could not find original source after build"
-      end
-    end
+  def download_non_build_dependency(url, dependency_name, file_extension)
+      system("curl #{url} -o #{binary_builder_dir}/#{dependency_name}-#{latest_build['version']}.#{file_extension}") or raise "Could not download #{url}"
   end
 
   def copy_binaries_to_output_directory
       FileUtils.cp_r(Dir["#{binary_builder_dir}/*.tgz", "#{binary_builder_dir}/*.tar.gz", "#{binary_builder_dir}/*.phar"], binary_artifacts_dir)
-      FileUtils.cp_r("#{binary_artifacts_dir}/build.tgz", original_source_code_dir)
   end
-
 
   def create_git_commit_msg
     version_built = latest_build['version']
 
-    ext = case binary_name
+    ext = case dependency
             when 'composer' then
               '*.phar'
-            when 'go' then
+            when 'go', 'dotnet' then
               '*.tar.gz'
             else
               '-*.tgz'
           end
 
-    filename = Dir["#{binary_builder_dir}/#{binary_name + ext}"].first
-    filename.match /(binary-builder\/.*)/
-    short_filename = $1
+    filename = Dir["#{binary_builder_dir}/#{dependency + ext}"].first
+    short_filename = File.basename(filename)
 
     md5sum = Digest::MD5.file(filename).hexdigest
     shasum = Digest::SHA256.file(filename).hexdigest
 
-    ci_skip = remaining_builds[binary_name].empty? && !is_automated
+    ci_skip = remaining_builds[dependency].empty? && !is_automated
 
-    git_msg = "Build #{binary_name} - #{version_built}\n\nfilename: #{short_filename}, md5: #{md5sum}, sha256: #{shasum}"
-    git_msg += "\n\nsource url: #{source_url}, source #{verification_type}: #{verification_value}"
+    git_msg = "Build #{dependency} - #{version_built}\n\n"
+
+    git_yaml = {
+      "filename" => short_filename,
+      'version' => version_built,
+      'md5' => md5sum,
+      'sha256' => shasum,
+      'source url' => source_url,
+      "source #{verification_type}" => verification_value
+    }
+
+    git_msg += git_yaml.to_yaml
+
     git_msg += "\n\n[ci skip]" if ci_skip
     git_msg
   end
@@ -145,9 +132,26 @@ class ConcourseBinaryBuilder
   end
 
   def commit_yaml_artifacts(git_msg)
-      builds_file = File.join(builds_dir, "#{binary_name}-builds.yml")
+    #don't change behavior for non-automated builds
+    if is_automated
+      #get latest version of <binary>-built.yml
+      add_ssh_key_and_update(built_dir)
+      built_file = File.join(built_dir, 'binary-built-output' ,"#{dependency}-built.yml")
+      built = YAML.load_file(built_file)
+      built_versions = built[dependency]
+
+      if dependency_version_not_built(built_versions)
+        built[dependency].push latest_build
+        built[dependency][-1]["timestamp"] = Time.now.utc.to_s
+
+        File.write(built_file, built.to_yaml)
+      end
+      commit_and_rsync(built_dir, builds_yaml_artifacts, git_msg, built_file)
+    else
+      builds_file = File.join(builds_dir, 'binary-builds', "#{dependency}-builds.yml")
       File.write(builds_file, remaining_builds.to_yaml)
       commit_and_rsync(builds_dir, builds_yaml_artifacts, git_msg, builds_file)
+    end
   end
 
   def run_binary_builder(flags)
@@ -155,13 +159,16 @@ class ConcourseBinaryBuilder
 
     Dir.chdir(binary_builder_dir) do
       output = `./bin/binary-builder #{flags}`
-      raise "Could not build" unless $?.success?
+      unless $?.success?
+        puts output
+        raise "Could not build"
+      end
     end
 
     output
   end
 
-  def add_ssh_key_and_update(dir, branch)
+  def add_ssh_key_and_update(dir)
     File.write("/tmp/git_ssh_key", git_ssh_key)
     system(<<-HEREDOC)
     eval "$(ssh-agent)"
@@ -174,22 +181,21 @@ class ConcourseBinaryBuilder
     ssh-add /tmp/git_ssh_key
     set -x
     cd #{dir}
-    git checkout #{branch}
     git pull -r
     HEREDOC
   end
 
   def is_automated
-    automated = %w(composer godep glide nginx node)
-    automated.include? binary_name
+    automated = %w(bower composer dotnet godep glide nginx node)
+    automated.include? dependency
   end
 
   def commit_and_rsync(in_dir, out_dir, git_msg, file)
 
     Dir.chdir(in_dir) do
 
-      GitClient.set_global_config('user.email', 'cf-buildpacks-eng@pivotal.io')
-      GitClient.set_global_config('user.name', 'CF Buildpacks Team CI Server')
+      GitClient.set_global_config('user.email', 'getourneau@pivotal.io')
+      GitClient.set_global_config('user.name', 'CF Buildpacks Builder Fork')
       GitClient.add_file(file)
       GitClient.safe_commit(git_msg)
 
---
+++ scripts/generate-manifest.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+#This Script only support PHP and PHP7
+
+require 'digest'
+require 'fileutils'
+require 'yaml'
+
+task_root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..','..'))
+require "#{task_root_dir}/buildpacks-ci/lib/git-client"
+
+
+
+
+manifest_file = File.join(Dir.pwd, 'buildpack', "manifest.yml")
+manifest_file_out = File.join(Dir.pwd, 'buildpack', "manifest-yahoo.yml")
+
+buildpack_manifest = YAML.load_file(manifest_file)
+buildpack_dl_repos = "https://s3-ap-northeast-1.amazonaws.com/buildpacks-binaries-bucket/concourse-binaries/"
+
+
+dependencies = buildpack_manifest['dependencies']
+grades = Hash.new(0)
+
+# Get all binaries from s3 bucket
+# List all binaries
+listed_binaries= %x[aws s3 ls ${BP_BINARIES}/concourse-binaries/ --recursive].split(/$/).map(&:strip).map(&:split)
+listed_binaries.select do |rest|
+  if !rest.empty?
+  /php7?-yahoo\/((php7?)-(.*)-linux-x64.(\d.*).tgz)/.match(rest[3]) {
+     md5_url = "#{buildpack_dl_repos}#{$2}-yahoo/#{$1}.md5"
+     md5 = %x[curl -s #{buildpack_dl_repos}#{$2}-yahoo/#{$1}.md5]
+  	 grades["php-#{$3}"] = {"binary"=>$2,"version"=>$3,"timestamp"=>$4,"file"=>$1, "md5"=>md5}
+  }
+  end
+end.count > 0
+
+# Check all manifest php / php7 and replace by the one in S3
+missing_dep = ""
+dependencies.select do |dep|
+  if grades.include?("#{dep["name"]}-#{dep["version"]}") and dep["name"] == "php" and !dep.empty?
+     p "Replacing " + dep["uri"] + " ===> " + buildpack_dl_repos + grades["#{dep["name"]}-#{dep["version"]}"]["binary"]+ "/" +grades["#{dep["name"]}-#{dep["version"]}"]["file"]
+      dep["uri"] = buildpack_dl_repos + grades["#{dep["name"]}-#{dep["version"]}"]["binary"]+ "-yahoo/" +grades["#{dep["name"]}-#{dep["version"]}"]["file"]
+      dep["md5"] = grades["#{dep["name"]}-#{dep["version"]}"]["md5"]
+  else
+    if dep["name"] == "php" then
+  	 missing_dep += "No replacement for #{dep["name"]}-#{dep["version"]}"
+   else
+     p "No replacement for #{dep["name"]}-#{dep["version"]}"
+   end
+  end
+end.count > 0
+
+
+if !missing_dep.empty? then
+   p missing_dep.split("\n")
+   exit 1
+end
+
+buildpack_manifest["dependencies"] =  dependencies
+File.open(manifest_file_out, "w") do |file|
+  file.write(buildpack_manifest.to_yaml)
+end
+
+
+def commit_and_rsync(in_dir, out_dir, git_msg, filename)
+  Dir.chdir(in_dir) do
+    GitClient.set_global_config('user.email', 'getourneau@pivotal.io')
+    GitClient.set_global_config('user.name', 'Gwenn custom buildpack')
+    GitClient.add_file(filename)
+    GitClient.checkout_branch("yahoo-fork")
+    GitClient.safe_commit(git_msg)
+    system("rsync -a #{in_dir}/ #{out_dir}")
+  end
+
+end
+final_message = "Adding Yahoo manifest file"
+commit_and_rsync(File.join(Dir.pwd, 'buildpack'),File.join(Dir.pwd, 'buildpack-repo-out'),final_message,"manifest-yahoo.yml")
